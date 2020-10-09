efb18313ca80db8f1456c9fa23b999bd03150481
4ecHok <89267225785@mail.ru>
2020-10-09 21:20:54 +0300

    Initial Commit

---
+++ VTB_cars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VTB_cars
+//
+//  Created by Сергей Цыганков on 09.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
