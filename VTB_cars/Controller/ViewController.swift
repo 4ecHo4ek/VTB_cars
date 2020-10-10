c469343ee296f46bd09dfc0a80225a9319ab0e2a
4ecHok <89267225785@mail.ru>
2020-10-10 20:25:34 +0300

    add connection while sending photo

--- VTB_cars/Controller/ViewController.swift
+++ VTB_cars/Controller/ViewController.swift
@@ -9,7 +9,43 @@ import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate {
 
-    var imageData = UIImage()
+    var imageData: UIImage? {
+        didSet {
+           
+            let imageToSend = imageData?.description
+            
+            let headers = [
+              "x-ibm-client-id": "a92943b7bb34f5a779859791088a91da",
+              "content-type": "application/json",
+              "accept": "application/json"
+            ]
+            let parameters = ["content": imageToSend as Any] as [String : Any]
+
+           
+            let postData = try? JSONSerialization.data(withJSONObject: parameters, options: [])
+           
+            
+            let request = NSMutableURLRequest(url: NSURL(string: "https://gw.hackathon.vtb.ru/vtb/hackathon/car-recognize")! as URL,
+                                                    cachePolicy: .useProtocolCachePolicy,
+                                                timeoutInterval: 10.0)
+            request.httpMethod = "POST"
+            request.allHTTPHeaderFields = headers
+            request.httpBody = postData!
+
+            let session = URLSession.shared
+            let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+              if (error != nil) {
+                print(error!)
+              } else {
+                let httpResponse = response as? HTTPURLResponse
+                print(httpResponse!)
+              }
+            })
+
+            dataTask.resume()
+            
+        }
+    }
     
     @IBOutlet weak var image: UIImageView! // удалить
     
@@ -20,30 +56,37 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
 
     @IBAction func useCamera(_ sender: UIButton) {
        
-            
-                let cameraIcon = UIImage(systemName: "camera")
-                let photoIcon = UIImage(systemName: "photo")
+        showCamera()
+
+       
                 
-                let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-                let camera = UIAlertAction(title: "Камера", style: .default) { _ in
-                    self.chooseImagePicker(fromSource: .camera)
-                }
-                camera.setValue(cameraIcon, forKey: "image")
-                camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
-                
-                let photo = UIAlertAction(title: "Библиотека фото", style: .default) { _ in
-                    self.chooseImagePicker(fromSource: .photoLibrary)
-                }
-                photo.setValue(photoIcon, forKey: "image")
-                photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
-                let cancel = UIAlertAction(title: "Cancel", style: .cancel)
-                
-                actionSheet.addAction(camera)
-                actionSheet.addAction(photo)
-                actionSheet.addAction(cancel)
-                
-                present(actionSheet, animated: true)
     }
+    
+   func showCamera() {
+        let cameraIcon = UIImage(systemName: "camera")
+        let photoIcon = UIImage(systemName: "photo")
+        
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let camera = UIAlertAction(title: "Камера", style: .default) { _ in
+            self.chooseImagePicker(fromSource: .camera)
+        }
+        camera.setValue(cameraIcon, forKey: "image")
+        camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+        
+        let photo = UIAlertAction(title: "Библиотека фото", style: .default) { _ in
+            self.chooseImagePicker(fromSource: .photoLibrary)
+        }
+        photo.setValue(photoIcon, forKey: "image")
+        photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        actionSheet.addAction(camera)
+        actionSheet.addAction(photo)
+        actionSheet.addAction(cancel)
+        
+        present(actionSheet, animated: true)
+    }
+    
     
 }
 
@@ -63,10 +106,6 @@ extension ViewController: UIImagePickerControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo
         info: [UIImagePickerController.InfoKey : Any]){
-    
-        imageData = info[.editedImage] as! UIImage
-        
-        image.image = imageData // удалить
-        dismiss(animated: true, completion: nil)
+        imageData = info[.editedImage] as? UIImage
     }
 }
---
+++ VTB_cars/Model/ModelManager.swift
@@ -0,0 +1,14 @@
+//
+//  ModelManager.swift
+//  VTB_cars
+//
+//  Created by Сергей Цыганков on 10.10.2020.
+//
+
+import Foundation
+
+class ModelManager {
+    
+    
+    
+}
